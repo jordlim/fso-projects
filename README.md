0c6495b3396c6a673e46eab6c2777c10564724ad
Jordan Lim <80717189+jordlim@users.noreply.github.com>
2024-12-10 20:07:43 +0000

    Complete exercise 1.6

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fso-unicafe
+
+Unicafe Project for Full Stack Open
+https://fullstackopen.com/en/
--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,15 +1,55 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return (
+    <h1>{props.section}</h1>
+  )
+}
+
+const Stat = ({ type, name }) => (
+  <div>
+    {name} {type}
+  </div>
+)
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good +1)
+  }
+  const handleBadClick = () => {
+    setBad(bad +1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral +1)
+  }
+
   return (
     <div>
-      code here
+      <Header section={"give feedback"} />
+      
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+
+      <Header section={"statistics"} />
+      <Stat type={good} name="good"/>
+      <Stat type={neutral} name="neutral"/>
+      <Stat type={bad} name="bad"/>
+      
     </div>
+    
   )
 }
 
